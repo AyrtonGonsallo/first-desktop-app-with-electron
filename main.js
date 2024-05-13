d368d4c3b4d4d67ee1d483d12893f2f227ab94cd
AyrtonGonsallo <ayrtongonsallo444@gmail.com>
2024-05-13 16:04:26 +0100

    fin v1

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-const { app, BrowserWindow } = require('electron');
-
+const { app, BrowserWindow ,Menu} = require('electron');
+const path = require('path');
 function createWindow() {
     const win = new BrowserWindow({
         width: 800,
@@ -10,6 +10,43 @@ function createWindow() {
     });
 
     win.loadFile('index.html');
+    // Création du menu
+    const mainMenu = Menu.buildFromTemplate([
+        {
+            label: 'File',
+            submenu: [
+                {
+                    label: 'Accueil',
+                    click() {
+                        win.loadFile('index.html');
+                    }
+                },
+                {
+                    label: 'Sports',
+                    click() {
+                        win.loadFile('sports.html');
+                    }
+                },
+                // Ajoutez d'autres éléments de menu pour les autres routes si nécessaire
+            ]
+        }
+    ]);
+
+    // Définition du menu comme menu de l'application
+    Menu.setApplicationMenu(mainMenu);
 }
 
 app.whenReady().then(createWindow);
+
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
+});
+
+app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+        createWindow();
+    }
+});
